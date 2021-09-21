8429219c078cbb370002b2a9dec543cdf13b0544
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-09-21 12:46:27 -0300

    Criar módulodb.ts para separar o gerenciamento do pool de conexões pelo cliente Pg do Postgres.

---
+++ src/db.ts
@@ -0,0 +1,19 @@
+import { Pool } from 'pg';
+
+
+
+const connectionString = '';
+
+// Trabalhar com um Pool de conexões
+const db = new Pool({ connectionString });
+
+
+
+
+export default db;
+
+
+
+
+
+
